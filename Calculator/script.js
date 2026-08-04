5e3b24d5d780dfed73ff7dfd1538ec6101d7d695
Minahil Amjad <minahilmirza9876@gmail.com>
2026-08-04 16:39:13 +0500

    Calculator

---
+++ Calculator/script.js
@@ -0,0 +1,26 @@
+// 1. Select the calculator screen from HTML
+const displayInput = document.getElementById("display");
+
+// 2. Append clicked number or operator to the screen
+function displaynumber(val) {
+  displayInput.value += val;
+}
+
+// 3. Clear the whole screen (AC)
+function clearDisplay() {
+  displayInput.value = "";
+}
+
+// 4. Delete the last character (DEL)
+function del() {
+  displayInput.value = displayInput.value.slice(0, -1);
+}
+
+// 5. Calculate the result (=)
+function calculate() {
+  try {
+    displayInput.value = eval(displayInput.value);
+  } catch (error) {
+    displayInput.value = "Error";
+  }
+}
---
+++ Calculator/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+var string=`My name is ${myName} and I am a software developer.`;
+console.log(string);
